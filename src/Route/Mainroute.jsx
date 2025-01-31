9e8c75bacf95753c73f0cbed932d9487148902b1
mubashirbappi <mubashirbappi@gmail.com>
2025-01-31 19:10:12 +0600

    project setup

---
+++ src/Route/Mainroute.jsx
@@ -0,0 +1,14 @@
+
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
+const Mainroute = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+  
+
+export default Mainroute;
