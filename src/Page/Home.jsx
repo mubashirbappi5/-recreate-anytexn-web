f3b2587aa6df79385b081f49f8d72db083bf052b
mubashirbappi <mubashirbappi@gmail.com>
2025-02-02 14:07:40 +0600

    phlosophy graph comaplate

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Components/Banner";
 import Client from "../Components/Client";
 import Legacy from "../Components/Legacy";
+import PHILOSOPHY from "../Components/PHILOSOPHY";
 import Trusted from "../Components/Trusted";
 
 
@@ -10,7 +11,7 @@ const Home = () => {
     return (
         <div>
             <Banner/>
-
+           <PHILOSOPHY/>
             <Trusted/>
            <Client/>
             <Legacy/>
